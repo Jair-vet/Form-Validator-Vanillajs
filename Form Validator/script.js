5399c3accc507db0c505d77705ac4bd77ebbd5bc
Jair <jairaceves56@gmail.com>
2023-01-19 13:00:45 -0600

    Form Works

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -19,15 +19,14 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(){
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // return re.test(String(email).toLowerCase())
     if (re.test(input.value.trim())) {
-        showSuccess(input);
+      showSuccess(input);
     } else {
-        showError(input, 'Email is not valid');
+      showError(input, 'Email is not valid');
     }
-}
+  }
 
 // Check requiered fields
 function checkRequired(inputArr) {
@@ -48,6 +47,13 @@ function checkLength(input, min, max){
         showError(input, `${getFieldName(input)} must be less than ${max} characters`)
     }else {
         showSuccess(input)  // pasmos el input
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
     }
 }
 
